15e27285b1c0b1405cf637e31876d78682176ce5
johan-berntsson7@hotmail.com <johan-berntsson7@hotmail.com>
2026-09-21 12:04:33 +0200

    la till spelare

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExerciseOnsdag2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Här skapar jag ett objekt av typen Spelare
+            Spelare spelare1 = new Spelare("Johan", 10);
+            spelare1.Springa();
+        }
+    }
+
+    // Här är klassen Spelare
+    public class Spelare
+    {
+        public string Namn { get; set; }
+        public int TrojNummer { get; set; }
+
+        public Spelare(string namn, int trojNummer)
+        {
+            Namn = namn;
+            TrojNummer = trojNummer;
+        }
+
+        public void Springa()
+        {
+            Console.WriteLine($"{Namn} springer");
+        }
+    }
+}
